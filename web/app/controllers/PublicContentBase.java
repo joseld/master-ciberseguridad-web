4c4d1365d1b27045bc2bd8dc246fd9790c963fa7
Jose Luis <joseld@users.noreply.github.com>
2018-04-30 12:38:05 +0200

    Update PublicContentBase.java
    Includes Password pattern validation

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,6 +12,15 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
+        // (?=.*[0-9]) a digit must occur at least once
+        //(?=.*[a-z]) a lower case letter must occur at least once
+        //(?=.*[A-Z]) an upper case letter must occur at least once
+        //.{8,} at least 8 characters
+        String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}";
+        if (!password.matches(pattern)){
+            badRequest();
+        }
+        
         User u = new User(username, HashUtils.getMd5(password), type, -1);
         u.save();
         registerComplete();
